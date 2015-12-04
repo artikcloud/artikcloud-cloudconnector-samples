ca4a016485f87488f36a864ed0de7b10e496da53
David Bernard <d.bernard1@ssi.samsung.com>
2015-12-04 10:47:29 +0100

    OM-1396 by default, https (self-signed certificate) is disabled

--- sample-instagram/src/test/groovy/utils/MyCloudConnectorRun.groovy
+++ sample-instagram/src/test/groovy/utils/MyCloudConnectorRun.groovy
@@ -7,12 +7,12 @@ class MyCloudConnectorRun {
   public static void main(String[] args) {
     // @param hostname - hostname to use (default "0.0.0.0")
     // @param port - http port to use (can be null => 9000)
-    // @param httpsPort - https port to use (can be null => no ssl), if not null a self signed certificat is used
+    // @param httpsPort - https port to use (can be null => no ssl), if not null and no keyStore defined then a self signed certificat is used
     // @param httpsKeyStorePath The path to the keystore containing the private key and certificate, if not provided generates a keystore for you (optional)
     // @param httpsKeyStoreType - The key store type, defaults to JKS (optional)
     // @param httpsKeyStorePassword - The password, defaults to a blank password (optional)
     // @param httpsKeyStoreAlgorithm - The key store algorithm, defaults to the platforms default algorithm (optional)
-    def srvCfg = SimpleHttpServer.makeServerConfig('localhost', 9080, 9083, null, null, null, null)
+    def srvCfg = SimpleHttpServer.makeServerConfig('localhost', 9080, null, null, null, null, null)
     def base = ((args.length > 0)? args[0] : MyCloudConnectorRun.getPackage().getName()).replace('.', '/')
     println('base : ' + base)
     def server = SimpleHttpServer.start(
