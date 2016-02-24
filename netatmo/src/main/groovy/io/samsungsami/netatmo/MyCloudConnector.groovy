2053bb74fe38c64f1334bfba60f3b5351190ca27
Adrien Broussolle <a.broussolle@ssi.samsung.com>
2016-02-24 12:14:59 +0100

    OM-1724 using transformJson

--- netatmo/src/main/groovy/io/samsungsami/netatmo/MyCloudConnector.groovy
+++ netatmo/src/main/groovy/io/samsungsami/netatmo/MyCloudConnector.groovy
@@ -36,18 +36,16 @@ class MyCloudConnector extends CloudConnector {
     @Override
     def  Or<ActionResponse, Failure> onAction(Context ctx, ActionDef action, DeviceInfo dInfo) {
         def req = new RequestDef(stationEndpoint)
+        DeviceSelector dSelector = new BySamiDeviceId(dInfo.did)
         def actionRequests = null
         switch (action.name) {
             case "getAllData":
-                actionRequests = [new ActionRequest(new BySamiDeviceId(dInfo.did),[req])]
+                actionRequests = [new ActionRequest(dSelector,[req])]
                 break
             case "getData":
                 def json = slurper.parseText(action.params)
-                if (json.stationId != null){
-                    actionRequests = [new ActionRequest(new BySamiDeviceId(dInfo.did),[req])].withQueryParams(["device_Id": json.stationId])
-                } else {
-                    return new Bad(new Failure("unsupported action for netatmo getData without stationId"))
-                }
+                def keyAndParams = [["device_Id", json.stationId]]
+                return buildActionResponse(req, dSelector, keyAndParams)
                 break
             default:
                 return new Bad(new Failure("unsupported action for netatmo:" + action.name))
@@ -71,58 +69,40 @@ class MyCloudConnector extends CloudConnector {
                         def ts = json.time_server
                         def events = json.body.devices.collectMany{ data ->
                             def netatmoId = data._id
-                            def modules = data?.modules ?: []
-                            def moduleEvents = modules.collectMany{ moduleData ->
-                                def windStr = moduleData?.dashboard_data?.WindStrength ?: null
-                                def windDir = moduleData?.dashboard_data?.WindAngle ?: null
-                                def gustStr = moduleData?.dashboard_data?.GustStrength ?: null
-                                def gustDir = moduleData?.dashboard_data?.GustAngle ?: null
-                                def dataType = (moduleData?.data_type ?: []).join(", ")
-                                def rain = moduleData?.dashboard_data?.Rain ?: null
-                                def rainSum1 = moduleData?.dashboard_data?.sum_rain_1 ?: null
-                                def rainSum24 = moduleData?.dashboard_data?.sum_rain_24 ?: null
-                                def battery = moduleData?.battery_vp ?: null
-                                def mType = moduleData?.type ?: null
-                                if ((mType != null) && (battery != null) && (dataType != null)) {
-                                    [ new Event(ts,
-                                            '''{"netatmoId": "''' + netatmoId + '''"'''
-                                                    + ''', "module": {"moduleType":"''' + mType + '''", "dataType":''' + optional(dataType) + ''', "battery":''' + battery
-                                                    + ''', "gustStrength":''' + gustStr + ''', "gustAngle":''' + gustDir
-                                                    + ''', "windStrength":''' + windStr + ''', "windAngle":''' + windDir
-                                                    + ''', "rain":''' + rain + ''', "rain1h":''' + rainSum1+ ''', "rain24h":''' + rainSum24
-                                                    + '''}}''')
-                                    ]
-                                } else {
-                                    []
-                                }
+                            def createEvent = { key, jsonEvent ->
+                                new Event(ts * 1000L,
+                                        JsonOutput.toJson(
+                                                ["netatmoId": netatmoId,
+                                                 (key):jsonEvent]
+                                        ).trim())
                             }
-                            def timeZ = data?.place?.timezone ?: null
-                            def city = data?.place?.city ?: null
-                            def alt = data?.place?.altitude ?: null
-                            def lat = data?.place?.location[0] ?: null
-                            def longi = data?.place?.location[1] ?: null
-                            def temp = data?.dashboard_data?.Temperature ?: null
-                            def noise = data?.dashboard_data?.Noise ?: null
-                            def humid = data?.dashboard_data?.Humidity ?: null
-                            def press = data?.dashboard_data?.Pressure ?: null
-                            def pressTrend = data?.dashboard_data?.pressure_trend ?: null
-                            def absPress = data?.dashboard_data?.AbsolutePressure ?: null
-                            def co2 = data?.dashboard_data?.CO2 ?: null
-                            def wifiStat = data?.wifi_status ?: null
-                            def tempTrend = data?.dashboard_data?.temp_trend ?: null
-                            def maxTemp = data?.dashboard_data?.max_temp ?: null
-                            def minTemp = data?.dashboard_data?.min_temp ?: null
-                            def dateMaxTemp = data?.dashboard_data?.date_max_temp ?: null
-                            def dateMinTemp = data?.dashboard_data?.date_min_temp ?: null
-                            def sType = data?.type ?: null
-                            moduleEvents + [new Event(ts, '''{"netatmoId": "''' + netatmoId + '''"'''
-                                    + ''', "station": {"city":''' + optional(city) + ''', "altitude":''' + alt + ''', "lat":''' + lat+ ''', "long":''' + longi + ''', "timeZone":''' + optional(timeZ)
-                                    + ''', "wifiStatus":''' + wifiStat+ ''', "stationType":''' + optional(sType)
-                                    + ''', "temp":''' + temp + ''', "humidity":''' + humid + ''', "co2":''' + co2 + ''', "noise":''' + noise
-                                    + ''', "pressure":''' + press + ''', "absolutePressure":''' + absPress
-                                    + ''', "pressureTrend":''' + optional(pressTrend) + ''', "tempTrend":''' + optional(tempTrend)
-                                    + ''', "maxTemp":''' + maxTemp + ''', "minTemp":''' + minTemp + ''', "dateMaxTemp":''' + dateMaxTemp + ''', "dateMinTemp":''' + dateMinTemp
-                                    + '''}}''')]
+
+                            def moduleEvents = data?.modules?.collect{ moduleData ->
+                                createEvent("module", transformJson(moduleData, { k, v ->
+                                    switch (k) {
+                                        case "type":        return ["moduleType": (v)]
+                                        case "data_type":   return ["dataType": v.join("'")]
+                                        default:            return [(k): (v)]
+                                    }
+                                }))
+                            }
+                            def stationEvent = [
+                                    createEvent("station", transformJson(data, { k, v ->
+                                        switch (k) {
+                                            case "modules":     return [:]
+                                            case "location":    return ["lat": v[0], "long": v[1]]
+                                            case "temp_trend":  return ["tempTrend": (v)]
+                                            case "max_temp":    return ["maxTemp": (v)]
+                                            case "min_temp":    return ["minTemp": (v)]
+                                            case "date_min_temp":   return ["dateMinTemp": (v)]
+                                            case "date_max_temp":   return ["dateMaxTemp": (v)]
+                                            case "Temperature": return ["temp": (v)]
+                                            case "type":        return ["stationType": (v)]
+                                            default:            return [(k): (v)]
+                                        }
+                                    }))
+                            ]
+                            stationEvent + moduleEvents
                         }
                         return new Good(events)
                         break
@@ -136,10 +116,30 @@ class MyCloudConnector extends CloudConnector {
         }
     }
 
-    private def String optional(String str){
-        if (str == null)
-            null
-        else
-            "\"" + str + "\""
+
+    private def buildActionResponse(RequestDef req, DeviceSelector dSelector, List<List<String>> keyAndParams) {
+        keyAndParams.collect{ keyAndParam ->
+            def param = keyAndParam[1]
+            if (param == null) {
+                return new Bad(new Failure("unsupported action for openWeatherMap getData without " + keyAndParam[1]))
+            }
+            def key = keyAndParam[0]
+            req = req.addQueryParams([(key):param])
+        }
+        new Good(new ActionResponse([new ActionRequest(dSelector,[req])]))
+    }
+
+    private def transformJson(obj, f) {
+        if(obj instanceof java.util.Map) {
+            obj.collectEntries { k, v ->
+                (v != null) ? f(k, transformJson(v, f)) : [:]
+            }
+        } else if(obj instanceof java.util.Collection) {
+            obj.collect{ item ->
+                transformJson(item, f)
+            }
+        } else {
+            obj
+        }
     }
 }
