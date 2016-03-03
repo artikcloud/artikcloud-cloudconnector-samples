5cdfdaecb0ece7398c5c212ca38b9972ae40aa19
Adrien Broussolle <a.broussolle@ssi.samsung.com>
2016-03-03 13:21:33 +0100

    OM-1724 openWeatherMap forecast
    --amend

--- open-weather-map/src/main/groovy/io/samsungsami/openWeatherMap/MyCloudConnector.groovy
+++ open-weather-map/src/main/groovy/io/samsungsami/openWeatherMap/MyCloudConnector.groovy
@@ -3,13 +3,34 @@ package io.samsungsami.openWeatherMap
 import com.samsung.sami.cloudconnector.api_v1.*
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
-import org.scalactic.*
+import org.scalactic.Bad
+import org.scalactic.Good
+import org.scalactic.Or
 
-import static java.net.HttpURLConnection.*
+import java.text.SimpleDateFormat
+
+import static java.net.HttpURLConnection.HTTP_OK
 
 class MyCloudConnector extends CloudConnector {
     static final String currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather"
+    static final String forecastWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast"
+    static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
     def JsonSlurper slurper = new JsonSlurper()
+    static final allowedKeys = [
+            "all",
+            "clouds","coord","country",
+            "date","deg","description","dt",
+            "humidity",
+            "id","icon",
+            "lat","long",
+            "main",
+            "name",
+            "pressure",
+            "rain",
+            "sys","sunrise","sunset","speed","snow",
+            "temp","temp_min","temp_max","three_hours",
+            "wind","weather"
+    ]
 
 
 
@@ -32,24 +53,43 @@ class MyCloudConnector extends CloudConnector {
     def  Or<ActionResponse, Failure> onAction(Context ctx, ActionDef action, DeviceInfo dInfo) {
         def dSelector = new BySamiDeviceId(dInfo.did)
         def json = slurper.parseText(action.params)
-        RequestDef req = new RequestDef(currentWeatherUrl).withQueryParams(["units":"metric"])
         switch (action.name) {
             case "getCurrentWeatherByCity":
+                RequestDef req = new RequestDef(currentWeatherUrl).withQueryParams(["units":"metric"])
                 if (json.city == null) {
                     return new Bad(new Failure("unsupported action for openWeatherMap getData without city " + action.params))
                 }
                 String city = json.city
                 if (json.countryCode != null){
-                   city = city + "," + json.countryCode
+                    city = city + "," + json.countryCode
                 }
                 req = req.addQueryParams(["q":city])
                 return new Good(new ActionResponse([new ActionRequest([req])]))
-                break
             case "getCurrentWeatherByGPSLocation":
+                RequestDef req = new RequestDef(currentWeatherUrl).withQueryParams(["units":"metric"])
                 def keyAndParams = [["lat", json?.lat],
-                                   ["lon",json?.long]]
+                                    ["lon",json?.long]]
                 return buildActionResponse(req, dSelector, keyAndParams)
-                break
+            case "getForecastWeatherByCity":
+                RequestDef req = new RequestDef(forecastWeatherUrl).withQueryParams(["units":"metric"])
+                if (json.city == null) {
+                    return new Bad(new Failure("unsupported action for openWeatherMap getData without city " + action.params))
+                }
+                String city = json.city
+                if (json.countryCode != null){
+                    city = city + "," + json.countryCode
+                }
+                if (json.daysToForecast == null) {
+                    return new Bad(new Failure("unsupported action for openWeatherMap getData without daysToForecast " + action.params))
+                }
+                req = req.addQueryParams(["q":city, "daysToForecast": (json?.daysToForecast)])
+                return new Good(new ActionResponse([new ActionRequest([req])]))
+            case "getForecastWeatherByGPSLocation":
+                RequestDef req = new RequestDef(forecastWeatherUrl).withQueryParams(["units":"metric"])
+                def keyAndParams = [["lat", json?.lat],
+                                    ["lon", json?.long],
+                                    ["daysToForecast", json?.daysToForecast]]
+                return buildActionResponse(req, dSelector, keyAndParams)
             default:
                 break
         }
@@ -59,35 +99,79 @@ class MyCloudConnector extends CloudConnector {
 
     @Override
     def Or<List<Event>, Failure> onFetchResponse(Context ctx, RequestDef req, DeviceInfo info, Response res) {
+        def jsonNow = ["date":"now"]
         switch(res.status) {
             case HTTP_OK:
                 switch(req.url) {
                     case currentWeatherUrl:
                         def json = slurper.parseText(res.content().trim())
-                        def msgWithoutWeather = transformJson(json, {k,v ->
-                            if (k == "lon")
-                                ["long":(v)]
-                            else if (k == "3h")
-                                ["three_hours":(v)]
-                            else if (k != "weather")
-                                [(k): (v)]
-                            else
-                                [:]
-                        })
-                        def events = json?.weather.collect{ weatherData ->
-                            def msg = msgWithoutWeather + ["weather": weatherData]
-                            new Event(msgWithoutWeather.dt, JsonOutput.toJson(msg).trim())
+                        return new Good(eventsForOneTimeFrame(json, json.dt  * 1000L, jsonNow))
+                    case forecastWeatherUrl:
+                        def json = slurper.parseText(res.content().trim())
+                        def daysToForecast = req.queryParams().get("daysToForecast").toInteger()
+                        Calendar cal = Calendar.getInstance()
+                        cal.add(Calendar.DATE,daysToForecast)
+                        def filter = dateFormat.format(cal.getTime())
+                        def city = weatherJsonTransFormation(json?.city)
+                       def ts = json?.list[0].dt * 1000L
+                        def events = json?.list.collectMany { prediction ->
+                            if (prediction?.dt_txt?.startsWith(filter) ?: false) {
+                                eventsForOneTimeFrame(prediction, ts, city)
+                            }
+                            else {
+                                []
+                            }
                         }
                         return new Good(events)
-                        break
                     default:
                         return new Bad(new Failure("receiving Responsse ${res} fron unknown request ${req}"))
-                        break
                 }
                 break
             default:
                 return new Bad(new Failure("[${info.did}] onFetchResponse got status http status : ${res.status()}) with content: ${res.content()}"))
         }
+    }
+
+
+    private def weatherJsonTransFormation(json){
+        transformJson(json, {k,v ->
+            if (k == "lon")
+                ["long":(v)]
+            else if (k == "dt_txt")
+                ["date":(v)]
+            else if (k == "3h")
+                ["three_hours":(v)]
+            else if (k != "weather")
+                [(k): (v)]
+            else
+                [:]
+        })
+    }
+    private def eventsForOneTimeFrame(json, ts, addToEvent) {
+        def msgWithoutWeather = weatherJsonTransFormation(json)
+        def events = json?.weather.collect{ weatherData ->
+            def msg = msgWithoutWeather + ["weather": weatherData] + addToEvent
+            new Event(ts, outputJson(msg))
+        }
+        return events
+    }
+
+
+
+    /*
+    HELPERS
+     */
+
+
+    private def outputJson(json){
+        JsonOutput.toJson(
+                transformJson(json, { k, v ->
+                    if (allowedKeys.contains(k))
+                        [(k): (v)]
+                    else
+                        [:]
+                })
+        ).trim()
     }
 
     private def buildActionResponse(RequestDef req, DeviceSelector dSelector, List<List<String>> keyAndParams) {
@@ -105,12 +189,23 @@ class MyCloudConnector extends CloudConnector {
     private def transformJson(obj, f) {
         if(obj instanceof java.util.Map) {
             obj.collectEntries { k, v ->
-                (v != null) ? f(k, transformJson(v, f)) : [:]
+                if (v != null){
+                    Object newV = transformJson(v, f)
+                    if (newV != [:])
+                    {
+                        f(k, newV)
+                    } else {
+                        [:]
+                    }
+                } else {
+                    [:]
+                }
             }
         } else if(obj instanceof java.util.Collection) {
-            obj.collect{ item ->
+            java.util.Collection newList = obj.collect{ item ->
                 transformJson(item, f)
             }
+            (newList.isEmpty()) ? [:] : newList
         } else {
             obj
         }
--- open-weather-map/src/test/groovy/io/samsungsami/openWeatherMap/MyCloudConnectorSpec.groovy
+++ open-weather-map/src/test/groovy/io/samsungsami/openWeatherMap/MyCloudConnectorSpec.groovy
@@ -1,13 +1,11 @@
 package io.samsungsami.openWeatherMap
 
 import com.samsung.sami.cloudconnector.api_v1.*
-import org.joda.time.*
 import org.scalactic.*
 import scala.Option
 import spock.lang.*
 import utils.*
 
-import static java.net.HttpURLConnection.*
 import static utils.Tools.*
 
 class MyCloudConnectorSpec extends Specification {
@@ -20,43 +18,49 @@ class MyCloudConnectorSpec extends Specification {
 	}
 	def extId = "23138311640030064"
 	def currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather"
+	static final String forecastWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast"
 	def info = new DeviceInfo("deviceId", Option.apply(extId), new Credentials(AuthType.OAuth2, "", "1j0v33o6c5b34cVPqIiB_M2LYb_iM5S9Vcy7Rx7jA2630pK7HIjEXvJoiE8V5rRF", Empty.option(), Option.apply("bearer"), ctx.scope(), Empty.option()), ctx.cloudId(), Empty.option())
 
 	def "on action fetch send request for current weather"() {
 		when:
 		def req = new RequestDef(currentWeatherUrl).withQueryParams(["units":"metric"])
+		def foreCastReq = new RequestDef(forecastWeatherUrl).withQueryParams(["units":"metric"])
 		def actionByCity = "getCurrentWeatherByCity"
 		def actionParam = '''{"countryCode":"fr","city":"cervieres"}'''
 		def actionParamNoCountryCode = '''{"city":"cervieres"}'''
 		def actionParamLatLong = '''{"lat":42,"long":42}'''
+		def actionParamLatLongPrediction = '''{"lat":42,"long":42,"daysToForecast":1}'''
 		def ts = 1441872018L
 		def actionDef = new ActionDef(Option.apply("sdid"), "ddid", ts, actionByCity, actionParam)
 		def actionDefNoCountryCode = new ActionDef(Option.apply("sdid"), "ddid", ts, actionByCity, actionParamNoCountryCode)
 		def actionDefLatLong = new ActionDef(Option.apply("sdid"), "ddid", ts, "getCurrentWeatherByGPSLocation", actionParamLatLong)
-		def res = [sut.onAction(ctx, actionDef, info)] + [sut.onAction(ctx, actionDefNoCountryCode, info)] + [sut.onAction(ctx, actionDefLatLong, info)]
+		def actionDefPrediction = new ActionDef(Option.apply("sdid"), "ddid", ts, "getForecastWeatherByGPSLocation", actionParamLatLongPrediction)
+		def res = [sut.onAction(ctx, actionDef, info)] + [sut.onAction(ctx, actionDefNoCountryCode, info)] + [sut.onAction(ctx, actionDefLatLong, info)] + [sut.onAction(ctx, actionDefPrediction, info)]
 		def expectedEvents = [
 				new ActionRequest([req.addQueryParams(["q":"cervieres,fr"])]),
 				new ActionRequest([req.addQueryParams(["q":"cervieres"])]),
 				new ActionRequest([req.addQueryParams(["lat":"42", "lon":"42"])]),
+				new ActionRequest([foreCastReq.addQueryParams(["lat":"42", "lon":"42","daysToForecast":"1"])]),
 		].collect{ actionReq -> new Good( new ActionResponse([actionReq]))}
 		then:
 		res[0].isGood()
 		res[0].get() == expectedEvents[0].get()
 		res[1].get() == expectedEvents[1].get()
 		res[2].get() == expectedEvents[2].get()
+		res[3].get() == expectedEvents[3].get()
 		res.size() == expectedEvents.size()
 		res == expectedEvents
 	}
 
-	def "create events from fetch response"() {
+	def "create events from fetch currentTime"() {
 		when:
 		def msg = readFile(this, "weather.json")
 		def fetchedResponse = new Response(HttpURLConnection.HTTP_OK, "application/json", msg)
 		def req = new RequestDef(currentWeatherUrl)
 		def res = sut.onFetchResponse(ctx, req, info,  fetchedResponse)
-		def ts = 1456914668
+		def ts = 1456914668000
 		def expectedEvents = [
-				new Event(ts, '''{"base":"cmc stations","clouds":{"all":8},"cod":200,"coord":{"lat":44.9,"long":6.65},"dt":1456914668,"id":3030142,"main":{"grnd_level":820.48,"humidity":89,"pressure":820.48,"sea_level":1023.01,"temp":277.85,"temp_max":277.85,"temp_min":277.85},"name":"Briancon","rain":{"three_hours":0},"sys":{"country":"FR","message":0.0028,"sunrise":1456898888,"sunset":1456939396},"wind":{"deg":254.009,"speed":1.01},"weather":{"description":"clear sky","icon":"02d","id":800,"main":"Clear"}}'''),
+				new Event(ts, '''{"clouds":{"all":8},"coord":{"lat":44.9,"long":6.65},"dt":1456914668,"id":3030142,"main":{"humidity":89,"pressure":820.48,"temp":277.85,"temp_max":277.85,"temp_min":277.85},"name":"Briancon","rain":{"three_hours":0},"sys":{"country":"FR","sunrise":1456898888,"sunset":1456939396},"wind":{"deg":254.009,"speed":1.01},"weather":{"description":"clear sky","icon":"02d","id":800,"main":"Clear"},"date":"now"}'''),
 		]
 		then:
 		res.isGood()
@@ -64,4 +68,61 @@ class MyCloudConnectorSpec extends Specification {
 		res.get().size() == expectedEvents.size()
 		res.get() == expectedEvents
 	}
+
+	def "do not send empty object after fetch"() {
+		when:
+		def msg = readFile(this, "weatherHole.json")
+		def fetchedResponse = new Response(HttpURLConnection.HTTP_OK, "application/json", msg)
+		def req = new RequestDef(currentWeatherUrl)
+		def res = sut.onFetchResponse(ctx, req, info,  fetchedResponse)
+		def ts = 1456914668000
+		def expectedEvents = [
+				new Event(ts, '''{"clouds":{"all":8},"coord":{"lat":44.9,"long":6.65},"dt":1456914668,"main":{"humidity":75,"pressure":863.98,"temp":3.74,"temp_max":4.55,"temp_min":3.74},"name":"Briancon","rain":{"three_hours":0},"wind":{"deg":254.009,"speed":1.01},"weather":{"description":"clear sky","icon":"02d","id":800,"main":"Clear"},"date":"now"}'''),
+		]
+		then:
+		res.isGood()
+		res.get()[0] == expectedEvents[0]
+		res.get().size() == expectedEvents.size()
+		res.get() == expectedEvents
+	}
+
+
+
+//UPDATE timestamp day in prediciton.json and test to replay this test
+/*
+	def "create events from fetch predictions"() {
+		when:
+		def msg = readFile(this, "prediction.json")
+		def fetchedResponse = new Response(HttpURLConnection.HTTP_OK, "application/json", msg)
+		def req = new RequestDef(forecastWeatherUrl).withQueryParams("daysToForecast":"0")
+		def req2 = new RequestDef(forecastWeatherUrl).withQueryParams("daysToForecast":"3")
+		def res = [sut.onFetchResponse(ctx, req, info,  fetchedResponse)]//, sut.onFetchResponse(ctx, req2, info,  fetchedResponse)]
+		def ts = 1456930800000
+		def expectedEvents0 = [
+				new Event(ts, '''{"clouds":{"all":92},"dt":1456930800,"date":"2016-03-02 15:00:00","main":{"grnd_level":996.01,"humidity":97,"pressure":996.01,"sea_level":1016.2,"temp":275.01,"temp_kf":-0.7,"temp_max":275.706,"temp_min":275.01},"rain":{"three_hours":1.065},"snow":{},"sys":{"population":0},"wind":{"deg":163,"speed":4.92},"weather":{"description":"light rain","icon":"10d","id":500,"main":"Rain"},"coord":{"lat":55.75222,"long":37.615555},"country":"RU","id":524901,"name":"Moscow","population":0}'''),
+				new Event(ts, '''{"clouds":{"all":92},"dt":1456941600,"date":"2016-03-02 18:00:00","main":{"grnd_level":995.92,"humidity":98,"pressure":995.92,"sea_level":1016.2,"temp":274.37,"temp_kf":-0.66,"temp_max":275.028,"temp_min":274.37},"rain":{"three_hours":4.5675},"snow":{},"sys":{"population":0},"wind":{"deg":174.504,"speed":4.76},"weather":{"description":"moderate rain","icon":"10n","id":501,"main":"Rain"},"coord":{"lat":55.75222,"long":37.615555},"country":"RU","id":524901,"name":"Moscow","population":0}'''),
+				new Event(ts, '''{"clouds":{"all":88},"dt":1456952400,"date":"2016-03-02 21:00:00","main":{"grnd_level":996.21,"humidity":100,"pressure":996.21,"sea_level":1016.54,"temp":273.72,"temp_kf":-0.62,"temp_max":274.345,"temp_min":273.72},"rain":{"three_hours":1.5825},"snow":{"three_hours":0.14},"sys":{"population":0},"wind":{"deg":215.003,"speed":3.51},"weather":{"description":"light rain","icon":"10n","id":500,"main":"Rain"},"coord":{"lat":55.75222,"long":37.615555},"country":"RU","id":524901,"name":"Moscow","population":0}''')
+		]
+		def expectedEvents1 = [
+				new Event(ts, '''{"clouds":{"all":92},"dt":1457136000,"date":"2016-03-05 00:00:00","main":{"grnd_level":995.19,"humidity":100,"pressure":995.19,"sea_level":1015.48,"temp":273.66,"temp_kf":0,"temp_max":273.66,"temp_min":273.66},"rain":{},"snow":{"three_hours":2.885},"sys":{"population":0},"wind":{"deg":90.5001,"speed":4.13},"weather":{"description":"snow","icon":"13n","id":601,"main":"Snow"},"coord":{"lat":55.75222,"long":37.615555},"country":"RU","id":524901,"name":"Moscow","population":0}'''),
+				new Event(ts, '''{"clouds":{"all":92},"dt":1457136000,"date":"2016-03-05 00:00:00","main":{"grnd_level":995.19,"humidity":100,"pressure":995.19,"sea_level":1015.48,"temp":273.66,"temp_kf":0,"temp_max":273.66,"temp_min":273.66},"rain":{},"snow":{"three_hours":2.885},"sys":{"population":0},"wind":{"deg":90.5001,"speed":4.13},"weather":{"description":"snow","icon":"13n","id":601,"main":"Snow"},"coord":{"lat":55.75222,"long":37.615555},"country":"RU","id":524901,"name":"Moscow","population":0}'''),
+				new Event(ts, '''{"clouds":{"all":92},"dt":1457136000,"date":"2016-03-05 00:00:00","main":{"grnd_level":995.19,"humidity":100,"pressure":995.19,"sea_level":1015.48,"temp":273.66,"temp_kf":0,"temp_max":273.66,"temp_min":273.66},"rain":{},"snow":{"three_hours":2.885},"sys":{"population":0},"wind":{"deg":90.5001,"speed":4.13},"weather":{"description":"snow","icon":"13n","id":601,"main":"Snow"},"coord":{"lat":55.75222,"long":37.615555},"country":"RU","id":524901,"name":"Moscow","population":0}'''),
+				new Event(ts, '''{"clouds":{"all":92},"dt":1457136000,"date":"2016-03-05 00:00:00","main":{"grnd_level":995.19,"humidity":100,"pressure":995.19,"sea_level":1015.48,"temp":273.66,"temp_kf":0,"temp_max":273.66,"temp_min":273.66},"rain":{},"snow":{"three_hours":2.885},"sys":{"population":0},"wind":{"deg":90.5001,"speed":4.13},"weather":{"description":"snow","icon":"13n","id":601,"main":"Snow"},"coord":{"lat":55.75222,"long":37.615555},"country":"RU","id":524901,"name":"Moscow","population":0}'''),
+				new Event(ts, '''{"clouds":{"all":92},"dt":1457136000,"date":"2016-03-05 00:00:00","main":{"grnd_level":995.19,"humidity":100,"pressure":995.19,"sea_level":1015.48,"temp":273.66,"temp_kf":0,"temp_max":273.66,"temp_min":273.66},"rain":{},"snow":{"three_hours":2.885},"sys":{"population":0},"wind":{"deg":90.5001,"speed":4.13},"weather":{"description":"snow","icon":"13n","id":601,"main":"Snow"},"coord":{"lat":55.75222,"long":37.615555},"country":"RU","id":524901,"name":"Moscow","population":0}'''),
+				new Event(ts, '''{"clouds":{"all":92},"dt":1457136000,"date":"2016-03-05 00:00:00","main":{"grnd_level":995.19,"humidity":100,"pressure":995.19,"sea_level":1015.48,"temp":273.66,"temp_kf":0,"temp_max":273.66,"temp_min":273.66},"rain":{},"snow":{"three_hours":2.885},"sys":{"population":0},"wind":{"deg":90.5001,"speed":4.13},"weather":{"description":"snow","icon":"13n","id":601,"main":"Snow"},"coord":{"lat":55.75222,"long":37.615555},"country":"RU","id":524901,"name":"Moscow","population":0}'''),
+				new Event(ts, '''{"clouds":{"all":92},"dt":1457136000,"date":"2016-03-05 00:00:00","main":{"grnd_level":995.19,"humidity":100,"pressure":995.19,"sea_level":1015.48,"temp":273.66,"temp_kf":0,"temp_max":273.66,"temp_min":273.66},"rain":{},"snow":{"three_hours":2.885},"sys":{"population":0},"wind":{"deg":90.5001,"speed":4.13},"weather":{"description":"snow","icon":"13n","id":601,"main":"Snow"},"coord":{"lat":55.75222,"long":37.615555},"country":"RU","id":524901,"name":"Moscow","population":0}'''),
+				new Event(ts, '''{"clouds":{"all":92},"dt":1457136000,"date":"2016-03-05 00:00:00","main":{"grnd_level":995.19,"humidity":100,"pressure":995.19,"sea_level":1015.48,"temp":273.66,"temp_kf":0,"temp_max":273.66,"temp_min":273.66},"rain":{},"snow":{"three_hours":2.885},"sys":{"population":0},"wind":{"deg":90.5001,"speed":4.13},"weather":{"description":"snow","icon":"13n","id":601,"main":"Snow"},"coord":{"lat":55.75222,"long":37.615555},"country":"RU","id":524901,"name":"Moscow","population":0}''')
+		]
+		then:
+		res[0].isGood()
+		res[0].get()[0] == expectedEvents0[0]
+		res[0].get()[1] == expectedEvents0[1]
+		res[0].get()[2] == expectedEvents0[2]
+		res[0].get().size() == expectedEvents0.size()
+		res[0].get() == expectedEvents0
+		res[0].isGood()
+		res[1].get()[0] == expectedEvents1[0]
+		res[1].get().size() == expectedEvents1.size()
+	}
+*/
 }
