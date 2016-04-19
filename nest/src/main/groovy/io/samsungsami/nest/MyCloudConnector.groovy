e635d8a3722065bf4e21087044c1858510fa2981
Dong W <dong.w@ssi.samsung.com>
2016-04-19 17:39:52 +0200

    OM-2199: implemented other Action except filtering getAllData

--- nest/src/main/groovy/io/samsungsami/nest/MyCloudConnector.groovy
+++ nest/src/main/groovy/io/samsungsami/nest/MyCloudConnector.groovy
@@ -1,6 +1,5 @@
 package io.samsungsami.nest
 
-
 import cloud.artik.cloudconnector.api_v1.*
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
@@ -21,94 +20,127 @@ class MyCloudConnector extends CloudConnector {
             "structure_name",
             "target_temperature_c", "target_temperature_high_c", "target_temperature_low_c"
     ]
-
-    static final String endpoint = "https://developer-api.nest.com"
-    static final String getAllEndpoint = "${endpoint}/all"
     def JsonSlurper slurper = new JsonSlurper()
 
-
     def Or<RequestDef, Failure> signAndPrepare(Context ctx, RequestDef req, DeviceInfo info, Phase phase) {
-        Map params = [:]
-        params.putAll(req.queryParams())
-        switch (phase) {
-            case Phase.undef:
-            case Phase.subscribe:
-            case Phase.unsubscribe:
-            case Phase.fetch:
-            ctx.debug(info.credentials.token)
-            return new Good(req.addHeaders(["Authorization": "Bearer " + info.credentials.token]))
-                break
-            case Phase.refreshToken:
-            case Phase.getOauth2Token:
-            default:
-                super.signAndPrepare(ctx, req, info, phase)
-        }
+        ctx.debug("signAndPrepare Started")
+        probe(ctx, info, "info")
+        ctx.debug('phase: ' + phase)
+        return new Good(req.addHeaders(['Authorization':'Bearer ' + info.credentials.token]))
     }
 
     @Override
-    def Or<ActionResponse, Failure> onAction(Context ctx, ActionDef action, DeviceInfo dInfo) {
+    def Or<ActionResponse, Failure> onAction(Context ctx, ActionDef action, DeviceInfo info) {
+        ctx.debug("onAction Started")
+        def json = slurper.parseText(action.params? action.params.trim(): "{}")
         switch (action.name) {
             case "getAllData":
-                return new Good(new ActionResponse([new ActionRequest([new RequestDef(getAllEndpoint)])]))
+                return new Good(new ActionResponse([new ActionRequest([new RequestDef("${ctx.parameters().endpoint}")])]))
+            case "setTemperature":
+                def querys = [  "did": json.deviceId,
+                                "target_temperature_c": json.temp
+                            ]
+                return nestJsonActionResponse(querys, "${ctx.parameters().endpoint}/devices/${ctx.parameters().productType}s/${querys.did}", ctx)
+                break
+            case "setTemperatureInFahrenheit":
+                def querys = [  "did": json.deviceId,
+                                "target_temperature_f": json.temp
+                            ]
+                return nestJsonActionResponse(querys, "${ctx.parameters().endpoint}/devices/${ctx.parameters().productType}s/${querys.did}", ctx)
+                break
+            case "setHome":
+                def querys = [  "sid": json.buildingId,
+                                "away": "home"
+                            ]
+                return nestJsonActionResponse(querys, "${ctx.parameters().endpoint}/structures/${querys.sid}", ctx)
+                break
+            case "setAway":
+                def querys = [  "sid": json.buildingId,
+                                "away": "away"
+                            ]
+                return nestJsonActionResponse(querys, "${ctx.parameters().endpoint}/structures/${querys.sid}", ctx)
+                break
             default:
                 return new Bad(new Failure("unsupported action for nest:" + action.name))
         }
     }
 
+    @Override
+    Or<RequestDef, Failure> fetch(Context ctx, RequestDef req, DeviceInfo info) {
+        probe(ctx, req, "req in Fetch")
+        new Good(req)
+    }
 
     @Override
     def Or<List<Event>, Failure> onFetchResponse(Context ctx, RequestDef req, DeviceInfo info, Response res) {
+        probe(ctx, res, "res")
         switch (res.status) {
             case HTTP_OK:
-                switch (req.url) {
-                    case getAllEndpoint:
-                        def json = slurper.parseText(res.content())
-
-
-                        return new Good([new Event(42L, outputJson(json))])
-                    default:
-                        return new Bad(new Failure("receiving Responsse ${res} fron unknown request ${req.req}"))
-                }
+                return new Good([new Event(42L, res.content)])
             default:
                 return new Bad(new Failure("[${info.did}] onFetchResponse got status http status : ${res.status()}) with content: ${res.content()}"))
         }
     }
 
-
-    private def buildActionResponse(RequestDef req, List<List<String>> keyAndParams) {
-        keyAndParams.collect { keyAndParam ->
-            def param = keyAndParam[1]
-            if (param == null) {
-                return new Bad(new Failure("unsupported action for openWeatherMap getData without " + keyAndParam[1]))
-            }
-            def key = keyAndParam[0]
-            req = req.addQueryParams([(key): param])
-        }
-        new Good(new ActionResponse([new ActionRequest([req])]))
-    }
-
-    private def transformJson(obj, f) {
-        if (obj instanceof java.util.Map) {
-            obj.collectEntries { k, v ->
-                (v != null) ? f(k, transformJson(v, f)) : [:]
-            }
-        } else if (obj instanceof java.util.Collection) {
-            obj.collect { item ->
-                transformJson(item, f)
-            }
-        } else {
-            obj
-        }
-    }
-
-    private def outputJson(json) {
-        JsonOutput.toJson(
-                transformJson(json, { k, v ->
-                    if (allowedKeys.contains(k))
-                        [(k): (v)]
-                    else
+    // Copy-pasted from sami-cloudconnector-samples/open-weather-map/src/main/groovy/io/samsungsami/openWeatherMap/MyCloudConnector.groovy -> transformJson
+    // applyToMessage(msg, f) will remove all empty values
+    def applyToMessage(msg, f) {
+        if (msg instanceof java.util.Map) {
+            msg.collectEntries { k, v ->
+                if (v != null) {
+                    def newV = applyToMessage(v, f)
+                    if (newV != [:]) {
+                        f(k, newV)
+                    } else {
                         [:]
-                })
-        ).trim()
+                    }
+                } else {
+                    [:]
+                }
+            }
+        } else if (msg instanceof java.util.Collection) {
+            java.util.Collection newList = msg.collect { item ->
+                applyToMessage(item, f)
+            }
+            (newList.isEmpty()) ? [:] : newList
+        } else {
+            msg
+        }
+    }
+
+    def filterByAllowedKeys(obj, keepingKeys) {
+        applyToMessage(obj, { k, v ->
+            keepingKeys.contains(k)? [(k): v]: [:]
+        })
+    }
+
+    def nestJsonActionResponse(paramsMap, url, ctx) {
+        if (nullValueCheck(paramsMap)) {
+            return new Bad(new Failure("Null value in " + paramsMap))
+        }
+        def paramsWithoutId = paramsMap.findAll{ k, v -> !k.endsWith('id') }
+        probe(ctx, paramsWithoutId, "paramsWithoutId")
+        def request = new RequestDef(url).withMethod(HttpMethod.Put)
+                        .withContent(JsonOutput.toJson(paramsWithoutId), "application/json")
+        return new Good(new ActionResponse([new ActionRequest([request])]))
+    }
+
+    def nullValueCheck(obj) {
+        switch(obj) {
+            case Map:
+                return obj.values().any{it == null}? true: false
+                break
+            case List:
+                return obj.any{it == null}? true: false
+                break
+            default:
+                return false
+            break
+        }
+    }
+
+
+    def probe(ctx, obj="", objName = "") {
+        ctx.debug(objName + " " + obj?.getClass() + "\n" + obj)
     }
 }
