3eb746228a2cef6c998ea90d1a0477518927efd8
David Bernard <d.bernard1@ssi.samsung.com>
2017-09-14 11:25:04 +0200

    runkeeper: update

--- runkeeper/src/test/groovy/cloudconnector/MyCloudConnectorSpec.groovy
+++ runkeeper/src/test/groovy/cloudconnector/MyCloudConnectorSpec.groovy
@@ -12,6 +12,7 @@ import cloud.artik.cloudconnector.api_v1.*
 import utils.FakeContext
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
+import static utils.Tools.*
 
 class MyCloudConnectorSpec extends Specification {
 
@@ -20,14 +21,14 @@ class MyCloudConnectorSpec extends Specification {
 
 		def info = new DeviceInfo(
 			"did",
-			Empty.option(), 
-			new Credentials(AuthType.OAuth2, "", "", Empty.option(), Option.apply("bearer"), ctx.scope(), Empty.option()), 
-			ctx.cloudId(), 
+			Empty.option(),
+			new Credentials(AuthType.OAuth2, "", "", Empty.option(), Option.apply("bearer"), ctx.scope(), Empty.option()),
+			ctx.cloudId(),
 			Empty.option()
 		)
 		def cloudUserData = '{"userId": 1, "weight": "/weight", "sleep": "/sleep", "fitness_activities": "/fitness"}'
 		def infoWithUserData = info.withUserData(JsonOutput.toJson(
-			["fitness_activities": "/fitness", "sleep": "/sleep", "userId" : 1, "weight": "/weight"])
+			["userId" : 1, "weight": "/weight", "sleep": "/sleep", "fitness_activities": "/fitness"])
 		)
 
 		def "store user data on onSubscribeResponse"() {
@@ -54,8 +55,8 @@ class MyCloudConnectorSpec extends Specification {
 						.withMethod(HttpMethod.Get)
 						.withQueryParams(["samiPullStartTs" : (ctx.now() - 3600*1000).toString(), "samiPullEndTs": ctx.now().toString()])
 					]
-				)	
-			]) 
+				)
+			])
 		}
 
 		def "push weight data to ARTIK Cloud on onFetchResponse"() {
@@ -65,7 +66,7 @@ class MyCloudConnectorSpec extends Specification {
 						.withQueryParams(["samiPullStartTs" : (ctx.now() - 3600*1000).toString(), "samiPullEndTs": ctx.now().toString()])
 			def body = JsonOutput.toJson([
 				"items": [
-					["timestamp": "Fri, 1 Jan 2016 09:44:00", "weight": 50], 
+					["timestamp": "Fri, 1 Jan 2016 09:44:00", "weight": 50],
 					["timestamp": "Fri, 1 Jan 2016 09:40:00", "free_mass": 10, "bmi": 5],
 					["timestamp": "Fri, 1 Jan 2016 08:30:00", "weight": 49]
 				]
@@ -78,10 +79,10 @@ class MyCloudConnectorSpec extends Specification {
 			def ts1 = DateTime.parse("Fri, 1 Jan 2016 09:44:00", MyCloudConnector.timestampFormat).getMillis()
 			def ts2 = DateTime.parse("Fri, 1 Jan 2016 09:40:00", MyCloudConnector.timestampFormat).getMillis()
 
-			result.get() == [
-				new Event(ts1, JsonOutput.toJson(["weight": 50])), 
+			cmpTasks(result.get(), [
+				new Event(ts1, JsonOutput.toJson(["weight": 50])),
 				new Event(ts2, JsonOutput.toJson(["bmi": 5, "free_mass": 10]))
-			]
+			])
 		}
 
 		def "push nothing when no new data on onFetchResponse"() {
@@ -119,9 +120,9 @@ class MyCloudConnectorSpec extends Specification {
 			result.isGood()
 			def ts1 = DateTime.parse("Fri, 1 Jan 2016 09:44:00", MyCloudConnector.timestampFormat).getMillis()
 
-			result.get() == [
+			cmpTasks(result.get(), [
 				new Event(ts1, JsonOutput.toJson(["max_heart_rate": 80, "min_heart_rate": 70, "total_distance": 40]))
-			]
+			])
 		}
 
 		def "push fitness data to ARTIK Cloud on onFetchResponse with no heart rate"() {
@@ -141,9 +142,9 @@ class MyCloudConnectorSpec extends Specification {
 			result.isGood()
 			def ts1 = DateTime.parse("Fri, 1 Jan 2016 09:44:00", MyCloudConnector.timestampFormat).getMillis()
 
-			result.get() == [
+			cmpTasks(result.get(), [
 				new Event(ts1, JsonOutput.toJson(["total_distance": 40]))
-			]
+			])
 		}
 
 		def "push fitness data to ARTIK Cloud on onFetchResponse with utc_offset"() {
@@ -163,9 +164,9 @@ class MyCloudConnectorSpec extends Specification {
 			result.isGood()
 			def ts1 = DateTime.parse("Fri, 1 Jan 2016 09:40:00", MyCloudConnector.timestampFormat).getMillis()
 
-			result.get() == [
+			cmpTasks(result.get(), [
 				new Event(ts1, JsonOutput.toJson(["total_distance": 40]))
-			]
+			])
 		}
 
 		def "push fitness data to ARTIK Cloud on onFetchResponse with utc_offset and duration"() {
@@ -185,9 +186,9 @@ class MyCloudConnectorSpec extends Specification {
 			result.isGood()
 			def ts1 = DateTime.parse("Fri, 1 Jan 2016 09:30:00", MyCloudConnector.timestampFormat).getMillis()
 
-			result.get() == [
+			cmpTasks(result.get(), [
 				new Event(ts1, JsonOutput.toJson(["duration": 30 * 60, "total_distance": 40]))
-			]
+			])
 		}
-		
+
 }
